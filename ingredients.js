cf92072d939a20c32e0a572363ab171d2847009e
joel-wowz <josephlafluer@hotmail.com>
2019-08-15 02:50:26 +0000

    ingredients arrays

---
+++ ingredients.js
@@ -0,0 +1,14 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+let i = 0;
+while (i < ingredients.length){
+  console.log(ingredients[i]);
+  i++;
+}
+for (var n = 0; n < ingredients.length; n++){
+  console.log(ingredients[n]);
+}
+
+for (var x = ingredients.length - 1; x >=0; x--){
+  console.log(ingredients[x]);
+}
+
